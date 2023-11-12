01a4bc7ff85a5cbaaebc05d362abf9bd4de3a8a3
AliEldewek <a.eldewik@nu.edu.eg>
2023-11-12 20:27:14 +0200

    Create LED.c

---
+++ MCAL/LED/src/LED.c
@@ -0,0 +1,20 @@
+#include "LED.h"
+
+void led_init(void){
+	
+	/* PIN 0 in the port is an output pin*/
+	
+	// the LED pin is initially off
+	
+	SET_BIT(LED_PORT_DIR,LED_PIN);
+	CLEAR_BIT(LED_PORT,LED_PIN);
+
+}
+
+void led_on(void){
+	SET_BIT(LED_PORT,LED_PIN);
+}
+
+void led_off(void){
+	CLEAR_BIT(LED_PORT,LED_PIN);
+}
