019ee9e1d88c987a04308647e07ead57e78c4459
AliEldewek <a.eldewik@nu.edu.eg>
2023-11-12 20:26:49 +0200

    Create buttons.c

---
+++ MCAL/buttons/src/buttons.c
@@ -0,0 +1,69 @@
+#include "Buttons.h"
+
+/* to initialize Buttons by making their pins as INPUT pins,
+enabling the internal pull-up resistor, and enabling the external interrupt
+for the ON/OFF button.*/
+
+
+void Buttons_init(void){
+	/* Setting the pins as INPUT pins*/
+	CLEAR_BIT(B_PORT_DIR,ON_OFF_PIN);
+	CLEAR_BIT(B_PORT_DIR,UP_PIN);
+	CLEAR_BIT(B_PORT_DIR,DOWN_PIN);	
+	
+	/*Enable internal Pull-Up Resistor*/
+	
+	SET_BIT(B_PORT,ON_OFF_PIN);
+	SET_BIT(B_PORT,UP_PIN);
+	SET_BIT(B_PORT,DOWN_PIN);
+	
+	
+	/*This Following Section is for the external interrupt
+	 assigned to the ON/OFF Button.*/
+	
+	/*Enable Global Interrupt bit*/
+	sei();
+	
+	/* Trigger with Rising Edge*/
+	SET_BIT(MCUCSR,ISC2);
+	
+	/*Enable INT2 (External Interrupt)*/
+	SET_BIT(GICR,INT2);
+}
+
+
+
+/*To check if UP button is pressed or not and return the result.*/
+
+
+unsigned char UP_pressed(void){
+	/* Check if pressed for the first time*/
+	if(!(READ_BIT(PINB,UP_PIN))){
+		/* wait for 50 ms to make sure it's pressed (debouncing solution)*/
+		_delay_ms(DEBOUNCE_INTERVAL);
+		
+		/* check one more time*/
+		if (!(READ_BIT(PINB,UP_PIN))){
+			return PRESSED;
+		}
+		else return NOT_PRESSED;
+	}
+	else return NOT_PRESSED;	
+}
+
+/* To check if DOWN button is pressed or not and return the result.*/
+
+unsigned char DOWN_pressed(void){
+	/* Check if pressed for the first time*/
+	if(!(READ_BIT(PINB,DOWN_PIN))){
+		/* wait for 50 ms to make sure it's pressed (debouncing solution)*/
+		_delay_ms(DEBOUNCE_INTERVAL);
+	
+		/*check one more time*/
+		if (!(READ_BIT(PINB,DOWN_PIN))){
+			return PRESSED;
+		}
+		else return NOT_PRESSED;
+	}
+	else return NOT_PRESSED;
+}
