752d9ecbfed09aa02e9bac721822cafbf0b0d0b5
AliEldewek <a.eldewik@nu.edu.eg>
2023-11-12 20:30:49 +0200

    Create makedep.mk

---
+++ Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+main.c
+
+MCAL\ADC\src\ADC.c
+
+MCAL\buttons\src\buttons.c
+
+MCAL\cool_heat_elements\src\Cool_Heat_Elements.c
+
+MCAL\EEPROM\src\EEPROM.c
+
+MCAL\LED\src\LED.c
+
+MCAL\SSD_DISP\src\SSD_disp.c
+
+MCAL\SYS\src\Sys.c
+
+MCAL\TIMER\src\Timer.c
+
