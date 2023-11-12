f21812f24f8c04999b4ebe82fc567fb13188ea0f
AliEldewek <a.eldewik@nu.edu.eg>
2023-11-12 20:25:31 +0200

    Create STD_TYPES.h

---
+++ library/std/STD_TYPES.h
@@ -0,0 +1,12 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+typedef unsigned int uint_32;
+typedef unsigned char uint_8;
+typedef signed int sint_32;
+typedef signed char sint_8;
+typedef unsigned short int uint_16;
+
+
+#endif /* STD_TYPES_H_ */
