fd3f913842ca31e14356c437c5405172ec597d60
AliEldewek <a.eldewik@nu.edu.eg>
2023-11-12 20:26:44 +0200

    Create ADC.c

---
+++ MCAL/ADC/src/ADC.c
@@ -0,0 +1,57 @@
+#include "ADC.h"
+
+
+
+/*to initialize the ADC to sense temperature from the sensor connected.*/
+
+void ADC_init(){
+	
+	/* Choosing reference voltage option.
+	the AVCC pin with a capacitor at the AREF pin.*/
+	SET_BIT(ADMUX,REFS0);
+	
+	/*Choosing Channel One (Single Ended In.) at A0 pin.*/
+	CLEAR_BIT(ADMUX,MUX0);
+	CLEAR_BIT(ADMUX,MUX1);
+	CLEAR_BIT(ADMUX,MUX2);
+	CLEAR_BIT(ADMUX,MUX3);
+	CLEAR_BIT(ADMUX,MUX4);
+	
+	/* Choosing the Data to be Right Adjusted in the Data Reg.*/
+	CLEAR_BIT(ADMUX,ADLAR);
+	
+	/* Choosing Prescaler for the ADC clock.
+	 the following is choosing (2) as a Prescaler.*/
+	SET_BIT(ADCSRA,ADPS0);
+	CLEAR_BIT(ADCSRA,ADPS1);
+	CLEAR_BIT(ADCSRA,ADPS2);
+	
+	/* Enable the ADC*/
+	SET_BIT(ADCSRA,ADEN);
+	
+}
+
+/* To read data from the temperature sensor when ever it's called.*/
+
+uint_16 ADC_read(){
+	
+	uint_16 data;
+	
+	/* Start Conversion*/
+	SET_BIT(ADCSRA,ADSC);
+	
+	// Wait for flag to be set.
+	// Indication of finished conversion
+	while(is_bit_clr(ADCSRA,ADIF));
+	
+	/* Read Data from the ADC Data Register
+	 Read the Low register first!*/
+	data = ADCL;
+	data |= (ADCH<<8);
+	
+	/* Clear Flag (As we are not using the interrupt)
+	 It won't be cleared automatically!*/
+	SET_BIT(ADCSRA,ADIF);
+	
+	return data;	
+}
